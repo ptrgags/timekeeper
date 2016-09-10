c41c4b89c09e0c0d42cdb47d3e8e06ec4a8d670e
Peter Gagliardi <ptrgags@live.com>
2016-09-10 19:07:48 -0400

    Convert current time to different timezones
    Convert the current time to different timezones.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,35 @@
+'use strict';
+
+var app = angular.module('timekeeper', [])
+app.controller('timekeeper', ["$scope", "$interval", ($scope, $interval) => {
+    $scope.now = moment();
+    $scope.timezone = "";
+    $scope.local = () => {
+        return $scope.now.local().format();
+    };
+    $scope.utc = () => {
+        return $scope.now.utc().format();
+    };
+    $scope.unix = () => {
+        return $scope.now.unix();
+    };
+    $scope.custom = () => {
+        if ($scope.timezone === "")
+            return "";
+        else
+            return $scope.now.tz($scope.timezone).format();
+    };
+
+    $interval(() => {
+        $scope.now = moment();
+    });
+}]);
+
 $(document).ready(() => {
-    var now = moment()
-    $("#test").html(now.format())
-    $("#test2").html(now.utc().format())
+    for (var tz of moment.tz.names()) {
+        var tz_option = $("<option>")
+            .attr("value", tz)
+            .text(tz);
+        $("#tz-select").append(tz_option);
+    }
 });
