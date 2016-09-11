7b84c51c11e0b1fe9bfeb06051554b12ddc0dbb0
Peter Gagliardi <ptrgags@live.com>
2016-09-10 20:53:19 -0400

    Put everything in a table
    Put everything in a nice table.
    
    Also added Swatch Internet Time just for fun.

--- scripts/main.js
+++ scripts/main.js
@@ -1,24 +1,35 @@
 'use strict';
 
+var FORMAT = "YYYY-MM-DD HH:mm:ss Z z"
+
 var app = angular.module('timekeeper', [])
 app.controller('timekeeper', ["$scope", "$interval", ($scope, $interval) => {
     $scope.now = moment();
     $scope.timezone = "";
     $scope.local = () => {
-        return $scope.now.local().format();
+        return $scope.now.local().format(FORMAT);
     };
     $scope.utc = () => {
-        return $scope.now.utc().format();
+        return $scope.now.utc().format(FORMAT);
     };
     $scope.unix = () => {
         return $scope.now.unix();
     };
     $scope.custom = () => {
         if ($scope.timezone === "")
-            return "";
+            return "N/A";
         else
-            return $scope.now.tz($scope.timezone).format();
+            return $scope.now.clone().tz($scope.timezone).format(FORMAT);
     };
+    $scope.swatch = () => {
+        var utc1 = $scope.now.clone().add(1, "hour")
+        var seconds = utc1.hours() * 3600
+            + utc1.minutes() * 60
+            + utc1.seconds();
+        var beats = Math.floor(seconds / 86.4);
+        var date = utc1.format("DD.MM.YYYY");
+        return `${date} @${beats}`;
+    }
 
     $interval(() => {
         $scope.now = moment();
