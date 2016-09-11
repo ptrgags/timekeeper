df733311dd792b8bfa6df5221f429163ccb05d16
Peter Gagliardi <ptrgags@live.com>
2016-09-11 09:25:40 -0400

    Add Timezone Comparison
    Added a panel for comparing timezones.

--- scripts/main.js
+++ scripts/main.js
@@ -16,7 +16,7 @@ app.controller('timekeeper', ["$scope", "$interval", ($scope, $interval) => {
         return $scope.now.unix();
     };
     $scope.custom = () => {
-        if ($scope.timezone === "")
+        if (!$scope.timezone)
             return "N/A";
         else
             return $scope.now.clone().tz($scope.timezone).format(FORMAT);
@@ -30,6 +30,43 @@ app.controller('timekeeper', ["$scope", "$interval", ($scope, $interval) => {
         var date = utc1.format("DD.MM.YYYY");
         return `${date} @${beats}`;
     }
+    $scope.tz_diff = () => {
+        var hours_diff = "N/A";
+        if ($scope.timezone_left && $scope.timezone_right) {
+            var left_offset = $scope.now.clone().tz($scope.timezone_left).utcOffset();
+            var right_offset = $scope.now.clone().tz($scope.timezone_right).utcOffset();
+            hours_diff = (right_offset - left_offset) / 60;
+        }
+
+        var left = $scope.timezone_left;
+        var right = $scope.timezone_right;
+        var diff_desc = "";
+        if (hours_diff === "N/A")
+            diff_desc = "N/A"
+        else if (hours_diff === 0)
+            diff_desc = `${left} is the same time as ${right}`;
+        else if (hours_diff < 0)
+            diff_desc = `${left} is ${Math.abs(hours_diff)} hours ahead of ${right}`;
+        else if (hours_diff > 0)
+            diff_desc = `${left} is ${hours_diff} hours behind ${right}`;
+
+        return diff_desc;
+    }
+    $scope.time_right = () => {
+        if (!$scope.datetime_left || !$scope.timezone_left || !$scope.timezone_right)
+            return "N/A"
+
+        //Date is in local time. Strip out timezone info
+        var date = $scope.datetime_left;
+        var naive = moment(date).format("YYYY-MM-DD HH:mm:ss");
+
+        //Construct with left timezone
+        var left_time = moment.tz(naive, "YYYY-MM-DD HH:mm:ss", $scope.timezone_left);
+
+        //Convert to right timezone and display
+        var right_time = left_time.tz($scope.timezone_right);
+        return right_time.format(FORMAT);
+    };
 
     $interval(() => {
         $scope.now = moment();
@@ -41,6 +78,10 @@ $(document).ready(() => {
         var tz_option = $("<option>")
             .attr("value", tz)
             .text(tz);
+        var tz_option_left = $(tz_option).clone();
+        var tz_option_right = $(tz_option).clone();
         $("#tz-select").append(tz_option);
+        $("#tz-select-left").append(tz_option_left);
+        $("#tz-select-right").append(tz_option_right);
     }
 });
